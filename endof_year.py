232dc72c693b3b3755af1d1d4cbc035a4fbb83b2
aliyev24 <x.rodriguez@mail.ru>
2025-01-03 08:27:20 +0400

    Last

--- endof_year.py
+++ endof_year.py
@@ -2,3 +2,7 @@ print('It is end of 2024.')
 a = 77
 b = 98
 print('This is the end of code.')
+
+q= 7
+b= 89
+c=q+b
--- imza.py
+++ imza.py
@@ -2,7 +2,8 @@ print('I should have a plan for today.')
 a = 96
 b=58
 c = a+b
-q=57
-print('I added this line at 04.56.')
+print(c)
+
 print('I made this change on 3rd of January at 04.16.')
 
+print('Marlos added this change at 08:50.')
